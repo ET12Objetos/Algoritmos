bd5d410d5f503999f29521b71d8ab8d82a5c6174
jvelazquez <jonathanvgms@gmail.com>
2023-03-16 09:28:26 -0300

    Write and read from console

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿//declara una variable entera con nombre 'numero'
+int numero;
+
+Console.Write("Ingrese un numero: ");
+//se lee por pantalla/consola un valor entero
+numero = Convert.ToInt32(Console.ReadLine());
+
+//informar por pantalla el valor ingresado por el usuario
+Console.WriteLine($"Ud. ha ingresado {numero}");
+
+//declaramos una variable de tipo string
+string mensaje;
+
+Console.WriteLine("Ingrese un mensaje:");
+
+//leemos una cadena de caracteres de la consola
+mensaje = Console.ReadLine();
+
+//informar el valor de la variable 'mensaje' por consola
+Console.WriteLine($"El mensaje ingresado es: {mensaje}");
+
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Ingrese un numero:");
+double numero1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Ingrese un numero:");
+double numero2 = Convert.ToDouble(Console.ReadLine());
+
+double suma = numero1 / numero2;
+
+//System.Console.WriteLine("Numero1: {0} y Numero2: {1}", numero1, numero2);
+//System.Console.WriteLine("La suma es: {0}", suma);
+
+System.Console.WriteLine($"La division es: {suma}");
