5a7d891897ecdfb6382861eab80b6b2df2f55530
jvelazquez <jonathanvgms@gmail.com>
2023-03-16 09:28:26 -0300

    Write and read from console

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("hace mucho calor, mucho");
+int contador = 1_000;
+float otro = 5.5f;
+double otroDouble = 13.49;
+
+string nombre = "Geronimo";
+bool haceCalor = true;
+
+Console.WriteLine(contador);
+Console.WriteLine($"El contador vale: {contador}");
