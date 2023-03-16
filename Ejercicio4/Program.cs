76e6f8839b2220a0491039658c593f52d11758c7
jvelazquez <jonathanvgms@gmail.com>
2023-03-16 11:41:45 -0300

    Add ex 6

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Ejercicio 1
+
+// Dados dos valores numéricos enteros a y b ingresados por pantalla,
+// calcular e informar la suma: a + b, la diferencia: a - b, 
+// y el producto: a * b entre dichos valores.
+
+Console.WriteLine("Ingrese un numero:");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese un numero:");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+int suma = numero1 + numero2;
+
+Console.WriteLine($"La suma es: {suma}");
+
+Console.WriteLine($"La diferencia es: {numero1 - numero2}");
+
+Console.WriteLine($"El producto es: {numero1 * numero2}");
---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Ingrese valor booleano: ");
+bool valor1 = bool.Parse(Console.ReadLine());
+Console.Write("Ingrese valor booleano: ");
+bool valor2 = Convert.ToBoolean(Console.ReadLine());
+
+//Con el operador ! se niega un valor booleano
+bool negacion1 = !valor1;
+bool negacion2 = !valor2;
+
+Console.WriteLine($"Valor1: {valor1}");
+Console.WriteLine($"Negacion Valor1: {negacion1}");
+Console.WriteLine($"Valor2: {valor2}");
+Console.WriteLine($"Negacion Valor2: {negacion2}");
+
+// Operador && se llama AND o conjuncion
+bool conjuncion = valor1 && valor2;
+Console.WriteLine($"Conjuncion: {conjuncion}");
+
+// Operador || se llama OR o disyuncion
+bool disyuncion = valor1 || valor2;
+Console.WriteLine($"Disyuncion: {disyuncion}");
+
