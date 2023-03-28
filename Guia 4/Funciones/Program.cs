44e39578a291931474f05dd5f31e98f1b94d17c0
jvelazquez <jonathanvgms@gmail.com>
2023-03-28 09:13:58 -0300

    Add functions example

---
+++ Guia 4/Funciones/Program.cs
@@ -0,0 +1,32 @@
+﻿//declarar una funcion
+//clasico
+int SumarClasico(int a, int b)
+{
+    //return numero1 + numero2;
+    int suma = a + b;
+
+    return suma;
+}
+
+//nuevo
+int SumarNuevo(int numero1, int numero2) => numero1 + numero2;
+
+//numero++;
+
+void Informar(int nro)
+{
+    Console.WriteLine($"El numero ingresado es: {nro}");
+}
+
+//=====================================================
+
+//programa principal
+Console.WriteLine("Ingrese un numero:");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese un numero:");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+int resultado = SumarClasico(numero1, numero2);
+
+Informar(resultado);
